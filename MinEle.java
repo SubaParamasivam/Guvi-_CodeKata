bb0237b45341d459195e1f39786ea4effacb2730
Suba Paramasivam <subaparaman21@gmail.com>
2018-02-17 06:01:17 -0800

    Commit

---
+++ MinEle.java
@@ -0,0 +1,48 @@
+import java.util.*;
+/**
+* <h1>print minimum element from given array</h1>
+* The MinEle program implements an application
+* that prints the minimum element from the given 
+* given array of integers and print it as 
+* output on the screen.
+*
+* <p>
+* 
+* Sample input : 4
+* Sample output: 1 7 4 2 
+*
+* @author  SubaParamasivam
+* @version 1.0
+* @since   2018-02-17
+*/
+class MinEle
+{
+	/**
+   * This is the main method which makes use
+   * @param args Unused.
+   * @return Nothing.
+   *
+   */
+	public static void main(String args[])
+	{
+		int n,a[],tmp;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		a=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					tmp=a[i];
+					a[i]=a[j];
+					a[j]=tmp;
+				}
+			}
+		}
+		System.out.println(a[n-1]);
+	}
+}
