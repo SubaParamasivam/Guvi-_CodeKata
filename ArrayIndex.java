0646d3c01a1f11819f6927747b71234987e5427b
Suba Paramasivam <subaparaman21@gmail.com>
2018-02-23 07:54:27 -0800

    Commit'

--- ArrayIndex.java
+++ ArrayIndex.java
@@ -1,6 +1,30 @@
 import java.util.*;
+/**
+* <h1>Print the array elements with index</h1>
+* The ArrayIndex program implements an application
+* that prints the  array element with its index as 
+* output on the screen.
+*
+* <p>
+* 
+* Sample input : 3
+                5 1 3
+* Sample output: 5 0
+                 1 1
+				 3 2
+*
+* @author  SubaParamasivam
+* @version 1.0
+* @since   2018-02-23
+*/
 public class ArrayIndex
 {
+	/**
+   * This is the main method which makes use
+   * @param args Unused.
+   * @return Nothing.
+   *
+   */
 	public static void main(String args[])
 	{
 		int n,a[];
@@ -10,6 +34,7 @@ public class ArrayIndex
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
-			System.out.print(a[i]+" "+i);
+			System.out.print(a[i]+" "+i+"\n");
+		}
 	}
 }
